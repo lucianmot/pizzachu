28cacf0c7984d1c7b7cde42c55e14507cf88cc03
lucianmot <lucian.i.mot@gmail.com>
2019-09-03 16:33:16 +0100

    fixing some npm start bug

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background-color: blue;
+    font-family: 'Open Sans', sans-serif;
   }
+
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+
 `
 
 function App() {
   return (
     <>
+      <h1>Pizzachu</h1>
       <GlobalStyle/>
       <div>Hello Pizzachu</div>
     </>
