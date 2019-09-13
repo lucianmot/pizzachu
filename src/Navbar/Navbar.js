23fc5c34c7cf5494bb13dd95c96448dba8ea42dc
lucianmot <lucian.i.mot@gmail.com>
2019-09-13 14:45:50 +0100

    added title to navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 import { pizzaRed } from "../Styles/colors";
+import {Title} from "../Styles/title"; 
 
-export const NavbarStyled = styled.div`
+const NavbarStyled = styled.div`
     background-color: ${pizzaRed};
     padding: 10px;
+`
+const Logo = styled(Title)`
+    font-sie: 20px;
+    color: white;
 `
 
 export function Navbar(){
     return <NavbarStyled>
-        Hello Navbar
+        <Logo>
+            Pizzachu🍕
+        </Logo>
     </NavbarStyled>;
 }
