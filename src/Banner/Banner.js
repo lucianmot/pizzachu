2ea1e768ec973aac334843d281159c6b98e7cbe8
lucianmot <lucian.i.mot@gmail.com>
2019-09-13 16:15:23 +0100

    added picture banner under navbar

---
+++ src/Banner/Banner.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Banner = styled.div`
+    height: 400px;
+    background-image: url("img/banner2.jpg");
+    background-position: center;
+    background-size: cover;
+`
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import { pizzaRed } from "../Styles/colors";
-import {Title} from "../Styles/title"; 
+import { Title } from "../Styles/title"; 
 
 const NavbarStyled = styled.div`
     background-color: ${pizzaRed};
-    padding: 10px;
+    padding: 15px;
+    position: fixed;
+    width: 100%;
 `
 const Logo = styled(Title)`
-    font-sie: 20px;
+    font-size: 20px;
     color: white;
     text-shadow: 2px 2px 4px #380502;
+    margin: auto;
+    width: 100%;
 `
 
 export function Navbar(){
     return <NavbarStyled>
         <Logo>
-            Pizzachu<span role="img" aria-label="pizza">🍕</span>
+            Pizzachu<span role="img" aria-label="pizza">🍕</span> - I cheese you!
         </Logo>
     </NavbarStyled>;
 }
